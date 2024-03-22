8326c2898cf7124681b2c2e851d0fa79a9131aa2
kuldeepdiggibyte <kp.managuli2000@gmail.com>
2024-03-22 21:19:37 +0530

    imported .csv file and added header

--- pyspark/dataframe/pyspark_dataframe.py
+++ pyspark/dataframe/pyspark_dataframe.py
@@ -7,4 +7,5 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('practice').getOrCreate()
 df_pyspark = spark.read.csv('expirence.csv')
 print(type(spark))
+
 spark.read.option('header','true').csv('expirence.csv').show()
